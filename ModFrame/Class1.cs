1a2f93b41613e10598d6c38992a210b1339c0f3b
sbtoonz <67915879+sbtoonz@users.noreply.github.com>
2021-12-20 08:29:23 -0700

    Update Class1.cs

--- ModFrame/Class1.cs
+++ ModFrame/Class1.cs
@@ -10,12 +10,19 @@ namespace ModFrame
         private const string ModName = "New Mod";
         private const string ModVersion = "1.0";
         private const string ModGUID = "some.new.guid";
-    
+        private static Harmony harmony = null!;
+        
         public void Awake()
         {
             Assembly assembly = Assembly.GetExecutingAssembly();
-            Harmony harmony = new(ModGUID);
+            harmony = new(ModGUID);
             harmony.PatchAll(assembly);
+        }
+        
+        
+        public void OnDestroy()
+        {
+            harmony.UnpatchSelf();
         }
     }
 }
