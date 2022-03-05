f0a0bb5818e4fd749392970cf2536119f51650dd
sbtoonz <67915879+sbtoonz@users.noreply.github.com>
2022-03-05 16:25:06 -0700

    Update Class1.cs

--- ModFrame/Class1.cs
+++ ModFrame/Class1.cs
@@ -18,11 +18,5 @@ namespace ModFrame
             harmony = new(ModGUID);
             harmony.PatchAll(assembly);
         }
-        
-        
-        public void OnDestroy()
-        {
-            harmony.UnpatchSelf();
-        }
     }
 }
